03f26870e68f734ca854620629e73ac05a11b49d
erikrios <erikriosetiawan49@gmail.com>
2020-03-16 11:57:53 +0700

    Change DatabaseMahasiswa class

--- app/src/main/java/com/erikriosetiawan/recyclerviewexercise/DatabaseMahasiswa.java
+++ app/src/main/java/com/erikriosetiawan/recyclerviewexercise/DatabaseMahasiswa.java
@@ -3,7 +3,7 @@ package com.erikriosetiawan.recyclerviewexercise;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = (Mahasiswa.class), version = 1)
+@Database(entities = {Mahasiswa.class}, version = 1)
 public abstract class DatabaseMahasiswa extends RoomDatabase {
 
     public abstract MahasiswaDao mahasiswaDao();
