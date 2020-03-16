3f510045d715dc032dcbdf00fdc66e51280242ef
erikrios <erikriosetiawan49@gmail.com>
2020-03-16 10:48:04 +0700

    Add mahasiswa class

---
+++ app/src/main/java/com/erikriosetiawan/recyclerviewexercise/Mahasiswa.java
@@ -0,0 +1,73 @@
+package com.erikriosetiawan.recyclerviewexercise;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "mahasiswa")
+public class Mahasiswa {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+
+    @ColumnInfo(name = "jurusan")
+    private String jurusan;
+
+    @ColumnInfo(name = "angkatan")
+    private int angkatan;
+
+    @ColumnInfo(name = "alamat")
+    private String alamat;
+
+    @ColumnInfo(name = "nomor_hp")
+    private String nomorHp;
+
+    public Mahasiswa(int id, String jurusan, int angkatan, String alamat, String nomorHp) {
+        this.id = id;
+        this.jurusan = jurusan;
+        this.angkatan = angkatan;
+        this.alamat = alamat;
+        this.nomorHp = nomorHp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public int getAngkatan() {
+        return angkatan;
+    }
+
+    public void setAngkatan(int angkatan) {
+        this.angkatan = angkatan;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNomorHp() {
+        return nomorHp;
+    }
+
+    public void setNomorHp(String nomorHp) {
+        this.nomorHp = nomorHp;
+    }
+}
